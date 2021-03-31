744a391693620571b2b25d0def86e53c89ba07aa
marshall-ku <marshall@kakao.com>
2021-03-31 11:47:58 +0900

    Update Difficulty Use button instead of li while it's clickable Add setN

--- src/ts/Difficulty.ts
+++ src/ts/Difficulty.ts
@@ -1,25 +1,26 @@
 import { app, resetApp } from "./app";
 
 function List(list: (string | number)[], setter: Function) {
-    const ul = document.createElement("ul");
+    const div = document.createElement("div");
 
     list.forEach((item) => {
-        const li = document.createElement("li");
+        const button = document.createElement("button");
 
-        li.innerText = `${item}`;
-        li.addEventListener("click", () => {
+        button.innerText = `${item}`;
+        button.addEventListener("click", () => {
             setter(item);
         });
 
-        ul.append(li);
+        div.append(button);
     });
 
-    return ul;
+    return div;
 }
 
 export default function Difficulty() {
     let type: string | undefined;
     let count: number | undefined;
+    let n: number | undefined;
     const setType = (string: string) => {
         type = string;
         typeList.remove();
@@ -27,12 +28,19 @@ export default function Difficulty() {
     };
     const setCount = (number: number) => {
         count = number;
+        countList.remove();
+        app.append(nList);
+    };
+    const setN = (number: number) => {
+        n = number;
+        console.log(type, count, n);
     };
     const typeList = List(
         ["숫자 비교", "덧셈", "뺄셈", "곱셈", "나눗셈"],
         setType
     );
     const countList = List([10, 20, 30, 40, 50], setCount);
+    const nList = List([2, 3, 4, 5, 6, 7, 8], setN);
 
     resetApp();
     app.append(typeList);
